e572de7a8e4855c99e485fefdce19df1a3b81c47
Thomas Francillette <22303411@me.uvsq.fr>
2025-02-17 10:39:33 +0100

    Ajout du package de test et de la classe de test PlanningCollectionsTest (pas implémentée encore)

---
+++ Planning/src/test/java/modele/PlanningCollectionsTest.java
@@ -0,0 +1,20 @@
+package modele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanningCollectionsTest {
+
+    @Test
+    void ajout() {
+    }
+
+    @Test
+    void getReservations() {
+    }
+
+    @Test
+    void testGetReservations() {
+    }
+}
